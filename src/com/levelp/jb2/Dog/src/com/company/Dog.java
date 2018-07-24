6f70b9d1f95a82283f34eb5d2f58812fbc836859
ryaboff99 <ryaboff99@gmail.com>
2018-07-24 15:20:18 +0300

    Dog & DogTestDrive classes

---
+++ src/com/levelp/jb2/Dog/src/com/company/Dog.java
@@ -0,0 +1,21 @@
+package com.levelp.jb2.Dog.src.com.company;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        int size;
+        String name;
+
+        void bark() {
+            if (size > 60) {
+                System.out.println("Woof! Woof!");
+            } else if (size > 14) {
+                System.out.println("Ruff! Ruff!");
+            } else {
+                System.out.println("Yip! Tip!")
+            }
+        }
+    }
+
+    }
+}
